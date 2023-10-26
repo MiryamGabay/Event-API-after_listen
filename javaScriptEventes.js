40efc1fc0756de1bb4607e1d69cc4490256f9aee
MiryamGabay <miryamgabay9@gmail.com>
2023-10-26 20:38:33 +0300

    my calendar events

---
+++ javaScriptEventes.js
@@ -0,0 +1,80 @@
+//create the calendar
+document.addEventListener('DOMContentLoaded', function () {
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth'
+    });
+    calendar.render();
+});
+
+
+//////////////////////API//////////////////////
+//GET
+document.addEventListener("DOMContentLoaded", getEvent())
+function getEvent() {
+    axios.get("https://eventsapi.onrender.com/api/Events").then((res) => {
+        var calendarEl = document.getElementById("calendar");
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+            initialView: "dayGridMonth",
+            events: res.data,
+        });
+        calendar.render();
+    });
+}
+
+
+//POST
+function addEvent(){
+    let date = prompt("enter date");
+    let title = prompt("enter title");
+    console.log(date)
+    console.log(title)
+    axios.post("https://eventsapi.onrender.com/api/Events", {
+      "title": title,
+      "start": date
+    }).then((res) => {
+      var calendarEl = document.getElementById('calendar');
+      var calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth',
+        events: res.data,
+      });
+      calendar.render();
+
+    });
+    getEvent();
+}
+
+
+//PUT
+function updateEvent(){
+    let id = prompt("enter id");
+    let date = prompt("enter date");
+    let title = prompt("enter title");
+    axios.put(`https://eventsapi.onrender.com/api/Events/${id}`, {
+      "title": title,
+      "start": date
+    }).then((res) => {
+      var calendarEl = document.getElementById('calendar');
+      var calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth',
+        events: res.data,
+      });
+      calendar.render();
+    });
+    getEvent()
+}
+
+
+//DELETE
+function deleteEvent(){
+    let id = prompt("enter id");
+    axios.delete(`https://eventsapi.onrender.com/api/Events/${id}`).then((res) => {
+      var calendarEl = document.getElementById('calendar');
+      var calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth',
+        events: res.data,
+      });
+      calendar.render();
+
+    });
+}
